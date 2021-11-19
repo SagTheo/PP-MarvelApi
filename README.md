5aca25700e8678d1e2a097b5813ebf810da91d39
SagTheo <sagniez.theo@laposte.net>
2021-11-19 12:55:40 +0100

    Styled the app, fixed the alignement of the list markers, made it responsive

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 This project will make use of an API (https://developer.marvel.com/)
     --> Goal here is to use and play around with fetch requests
+
+
+Difficulties encountered:
+    -figuring out how API requests work
+    -aligning vertically the marker of a list item and its text
+        -sorted out: dynamically included the marker as an icon in an img tag, then used the 
+                     'vertical-align' property with the value 'top' to align the text with the icon
--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ const attribution = document.querySelector('.attribution')
 const err = document.querySelector('.err')
 const comics = document.querySelector('.comics')
 const introList = document.querySelector('.introList')
+const charInfo = document.querySelector('.charInfo')
 
 // Empties the content of the description paragraph and the list of comics
 const emptyRes = () => {
@@ -18,6 +19,8 @@ const emptyRes = () => {
 // Displays the description of the character and a list of 10 comics that character is in
 // Handles errors
 btn.addEventListener('click', () => {
+    charInfo.style.display = 'none'
+    attribution.style.display = 'none'
     attribution.textContent = ''
     fetch(`https://gateway.marvel.com/v1/public/characters?name=${userInput.value}&apikey=${pubKey}`)
         .then(response => {
@@ -37,11 +40,19 @@ btn.addEventListener('click', () => {
                 introList.textContent = `Here is a list of 10 comics ${characterName} appeared in:`
                 for (let i = 0; i < comicsList.length / 2; i++) {
                     let li = document.createElement('li')
-                    li.textContent = comicsList[i].name
+                    let icon = document.createElement('img')
+                    let span = document.createElement('span')
+                    icon.setAttribute('src', 'icon.svg')
+                    icon.setAttribute('alt', 'marker')
+                    span.textContent = comicsList[i].name
+                    li.append(icon)
+                    li.append(span)
                     comics.append(li) 
                 }
 
+                charInfo.style.display = 'block'
                 attribution.textContent = 'Data provided by Marvel. © 2014 Marvel'
+                attribution.style.display = 'block'
                 err.textContent = ''
             } else {
                 err.textContent = 'Sorry, no description available for this character.'
